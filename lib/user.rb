76abc69236f02c030bbe001672a6bd9e6e351766
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-24 20:19:49 -0500

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,22 @@
+class User
+attr_accessor :first_name, :last_name  
+
+@@all = []
+
+def initialize
+    @first_name
+    @last_name 
+end 
+
+    # def first_name(first)
+    #     @first_name=first
+    # end 
+
+    # def last_name(last)
+    #     @last_name=last
+    # end 
+
+    def self.all
+        @@all 
+    end 
+end 
